284aa19f7805b3df05d2b0848a643c4fb57a7e03
vtrhp <vtrhp@hotmail.com>
2021-03-22 18:07:39 -0300

    Primeiro commit

---
+++ src/main/java/br/com/vtrhp/aberturaDeContas/AberturaDeContasApplication.java
@@ -0,0 +1,13 @@
+package br.com.vtrhp.aberturaDeContas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AberturaDeContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AberturaDeContasApplication.class, args);
+	}
+
+}
